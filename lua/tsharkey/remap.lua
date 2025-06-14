04e592516bb3c4e9b47b703e53a39595b3dcb982
tsharkey <sharkey.thomasm@gmail.com>
2025-06-14 08:21:35 -0400

    window navigation

--- lua/tsharkey/remap.lua
+++ lua/tsharkey/remap.lua
@@ -28,3 +28,9 @@ vim.keymap.set(
 )
 
 vim.keymap.set("n", "<leader>ss", "<cmd>Telescope grep_string<cr>")
+
+-- Window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
