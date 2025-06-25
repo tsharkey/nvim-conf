3b4728c92f035db30fb6d2904cc36b8cbf03c7ac
tsharkey <sharkey.thomasm@gmail.com>
2025-06-25 09:49:32 -0400

    use default config

--- lua/tsharkey/lazy/lint.lua
+++ lua/tsharkey/lazy/lint.lua
@@ -1,21 +1,12 @@
 return {
 	"mfussenegger/nvim-lint",
 	config = function()
+		vim.g.lint_debug = true
 		require("lint").linters_by_ft = {
 			astro = { "eslint" },
 			go = { "golangcilint" },
 			javascript = { "eslint" },
 			typescript = { "eslint" },
-		}
-		local golangcilint = require("lint.linters.golangcilint")
-		golangcilint.append_fname = true -- This is often default
-		golangcilint.args = {
-			"run",
-			"--out-format",
-			"json", -- Changed this line
-			"--issues-exit-code=0",
-			"--show-stats=false", -- This flag is fine
-			-- Potentially add other golangci-lint flags you need here
 		}
 	end,
 }
